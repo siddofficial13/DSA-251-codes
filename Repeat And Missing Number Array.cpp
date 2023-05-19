fc8e728aaafc68dc06ec7b83e7c89026b43f0b7b
Siddharth <128206073+siddofficial13@users.noreply.github.com>
2023-05-19 17:52:15 +0530

    Update Repeat And Missing Number Array.cpp

--- Repeat And Missing Number Array.cpp
+++ Repeat And Missing Number Array.cpp
@@ -1,35 +1,49 @@
-#include <bits/stdc++.h> 
-int kadane(int a[], int n){
-	int maxsum = INT_MIN;
-	int currsum = 0;
-	for(int i=0; i<n; i++){
-		currsum += a[i];
-
-		if(currsum < 0)
-			currsum = 0;
-		else if(currsum > maxsum)
-			maxsum = currsum;
-    }
-    
-    return (maxsum < 0) ? *max_element(a, a+n) : maxsum;
-}
-int maxSumRectangle(vector<vector<int>>& arr, int n, int m)
+#include <bits/stdc++.h>
+#define ll long long
+pair<int,int> missingAndRepeating(vector<int> &A, int n)
 {
-	int temp[n];
-	int maxsum = INT_MIN, currsum;
+	// Write your code here 
+	// vector<int>v;
+	// for(int i=0;i<n;i++){
+	// 	v.push_back(i+1);
+	// }
+	// int actsum=((n)*(n+1))/2;
+	// int sum=0;
+	// for(int i=0;i<n;i++){
+	// 	sum+=arr[i];
+	// }
+	// int miss=0;
+	// int rep=0;
+	// sort(arr.begin(),arr.end());
+	// for(int i=0;i<n;i++){
+	// 	if(arr[i]!=i+1 && arr[i]!=arr[i-1]){
+	// 		miss=i+1;
+	// 		break;
+	// 	}
+	// 	if(arr[i]!=i+1 && arr[i]==arr[i-1]){
+	// 		rep=arr[i];
+	// 		break;
+	// 	}
+	// }
 
-	for(int cstart = 0; cstart < m; cstart++){
-		memset(temp, 0, sizeof(temp));
-
-                for (int cend = cstart; cend < m; cend++) {
-
-                  for (int i = 0; i < n; i++)
-                    temp[i] += arr[i][cend];
-
-                  currsum = kadane(temp, n);
-                  maxsum = max(currsum, maxsum);
-                }
-        }
-
-        return maxsum;
+	// if(rep==0){
+	// 	return {miss,sum+miss-actsum};
+	// }
+	// else{
+	// 	return {actsum-sum+rep,rep};
+	// }
+    pair<int,int>ans;
+	ll num = n;
+    ll S=(num*(num+1))/2;
+    ll P=(num*(num+1)*(2*num+1))/6;
+    ll miss=0, rep=0;         
+    for(int i=0;i<A.size();i++){    
+	    S-=(ll)A[i];    
+		P-=(ll)A[i]*(ll)A[i];  
+	}
+    miss=(S+P/S)/2;
+    rep=miss-S;
+    ans.first=miss;    
+    ans.second=rep; 
+    return ans;
 }
