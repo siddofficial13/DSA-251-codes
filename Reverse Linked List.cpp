91ec035f439f23599c3cb7e1131ab2f52baec4f7
Siddharth <128206073+siddofficial13@users.noreply.github.com>
2023-05-21 17:10:54 +0530

    Create Reverse Linked List.cpp

---
+++ Reverse Linked List.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head) 
+{
+    // Write your code here
+    LinkedListNode<int> *dummy=NULL;
+    while(head!=NULL){
+        LinkedListNode<int> *nex=head->next;
+        head->next=dummy;
+        dummy=head;
+        head=nex;
+    }
+
+    return dummy;
+
+
+}
