ff14a9e80d786e8ff8a18c6ff8c7c005f1c0a03a
Siddharth <128206073+siddofficial13@users.noreply.github.com>
2023-05-22 18:02:50 +0530

    Create Quick Sort.cpp

---
+++ Quick Sort.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h> 
+
+ 
+
+int partition( vector<int>& arr, int s, int e) {
+
+ 
+
+    int pivot = arr[s];
+
+ 
+
+    int cnt = 0;
+
+    for(int i = s+1; i<=e; i++) {
+
+        if(arr[i] <=pivot) {
+
+            cnt++;
+
+        }
+
+    }
+
+ 
+
+    //place pivot at right position
+
+    int pivotIndex = s + cnt;
+
+    swap(arr[pivotIndex], arr[s]);
+
+ 
+
+    //left and right wala part smbhal lete h 
+
+    int i = s, j = e;
+
+ 
+
+    while(i < pivotIndex && j > pivotIndex) {
+
+ 
+
+        while(arr[i] <= pivot) 
+
+        {
+
+            i++;
+
+        }
+
+ 
+
+        while(arr[j] > pivot) {
+
+            j--;
+
+        }
+
+ 
+
+        if(i < pivotIndex && j > pivotIndex) {
+
+            swap(arr[i++], arr[j--]);
+
+        }
+
+ 
+
+    }
+
+ 
+
+    return pivotIndex;
+
+ 
+
+}
+
+ 
+
+void solve(vector<int>& arr, int s, int e) {
+
+ 
+
+    //base case
+
+    if(s >= e) 
+
+        return ;
+
+ 
+
+    //partitioon karenfe
+
+    int p = partition(arr, s, e);
+
+ 
+
+    //left part sort karo
+
+    solve(arr, s, p-1);
+
+ 
+
+    //right wala part sort karo
+
+    solve(arr, p+1, e);
+
+ 
+
+}
+
+vector<int> quickSort(vector<int> arr)
+
+{
+
+  solve(arr,0,arr.size()-1);
+
+  return arr;
+
+ 
+
+}
+
