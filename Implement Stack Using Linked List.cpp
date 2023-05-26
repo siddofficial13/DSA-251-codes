e1a43de8d0072b0e5c6ee787725e833e415aeb0b
Siddharth <128206073+siddofficial13@users.noreply.github.com>
2023-05-26 13:20:27 +0530

    Create Implement Stack Using Linked List.cpp

---
+++ Implement Stack Using Linked List.cpp
@@ -0,0 +1,68 @@
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+                int data;
+                Node *next;
+                Node(int data)
+                {
+                        this->data = data;
+                        this->next = NULL;
+                }
+        };
+
+*****************************************************************/
+
+class Stack {
+  // Write your code here
+   Node *top;
+   int size;
+public:
+  Stack() {
+    // Write your code here
+    top = NULL;
+    size = 0;
+  }
+
+  int getSize() {
+    // Write your code here
+    return size;
+  }
+
+  bool isEmpty() {
+    // Write your code here
+    return size == 0;
+  }
+
+  void push(int x) {
+    // Write your code here
+
+    Node *newnode = new Node(x);
+    newnode->next = top;
+    top = newnode;
+    size++;
+  }
+
+  void pop() {
+    // Write your code here
+    // if (top == nullptr)
+    if(size == 0)  return ;
+     
+
+    Node *temp = top;
+    // int ans = top->data;
+    top = top->next;
+    delete temp;
+    size--;
+    // return ans;
+  }
+
+  int getTop() {
+    // Write your code here
+    if(top == nullptr) return -1;
+    return top->data;
+  }
+};
