bc7c775187bf8bdfe88bf4bdfb2f20d59663ca83
Siddharth <128206073+siddofficial13@users.noreply.github.com>
2023-05-27 19:03:17 +0530

    Create Largest Rectangle In Histogram.cpp

---
+++ Largest Rectangle In Histogram.cpp
@@ -0,0 +1,81 @@
+//Better Approach
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution {
+  public:
+    int largestRectangleArea(vector < int > & heights) {
+      int n = heights.size();
+      stack < int > st;
+      int leftsmall[n], rightsmall[n];
+      for (int i = 0; i < n; i++) {
+        while (!st.empty() && heights[st.top()] >= heights[i]) {
+          st.pop();
+        }
+        if (st.empty())
+          leftsmall[i] = 0;
+        else
+          leftsmall[i] = st.top() + 1;
+        st.push(i);
+      }
+      // clear the stack to be re-used
+      while (!st.empty())
+        st.pop();
+
+      for (int i = n - 1; i >= 0; i--) {
+        while (!st.empty() && heights[st.top()] >= heights[i])
+          st.pop();
+
+        if (st.empty())
+          rightsmall[i] = n - 1;
+        else
+          rightsmall[i] = st.top() - 1;
+
+        st.push(i);
+      }
+      int maxA = 0;
+      for (int i = 0; i < n; i++) {
+        maxA = max(maxA, heights[i] * (rightsmall[i] - leftsmall[i] + 1));
+      }
+      return maxA;
+    }
+};
+int main() {
+  vector<int> heights = {2, 1, 5, 6, 2, 3, 1};
+  Solution obj;
+  cout << "The largest area in the histogram is " << obj.largestRectangleArea(heights); 
+  return 0;
+}
+
+//Optimal Approach
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution {
+  public:
+    int largestRectangleArea(vector < int > & histo) {
+      stack < int > st;
+      int maxA = 0;
+      int n = histo.size();
+      for (int i = 0; i <= n; i++) {
+        while (!st.empty() && (i == n || histo[st.top()] >= histo[i])) {
+          int height = histo[st.top()];
+          st.pop();
+          int width;
+          if (st.empty())
+            width = i;
+          else
+            width = i - st.top() - 1;
+          maxA = max(maxA, width * height);
+        }
+        st.push(i);
+      }
+      return maxA;
+    }
+};
+int main() {
+  vector < int > histo = {2, 1, 5, 6, 2, 3, 1};
+  Solution obj;
+  cout << "The largest area in the histogram is " << obj.largestRectangleArea(histo) << endl;
+  return 0;
+}
