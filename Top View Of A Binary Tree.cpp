693876af0f55c773d47bcb1987feb2e27b2ab121
Siddharth <128206073+siddofficial13@users.noreply.github.com>
2023-05-29 18:04:27 +0530

    Create Top View Of A Binary Tree.cpp

---
+++ Top View Of A Binary Tree.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the TreeNode class structure:
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T val;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+vector<int> getTopView(TreeNode<int> *root) {
+    // Write your code here.
+    vector<int>ans;
+    if(root==NULL)return ans;
+    map<int,int>mp;
+    queue<pair<TreeNode<int> *,int>>q;
+    q.push({root,0});
+    while(!q.empty()){
+        auto it=q.front();
+        q.pop();
+        TreeNode<int> *node =it.first;
+        int line=it.second;
+        if(mp.find(line)==mp.end()){
+            mp[line]=node->val;
+        }
+        if(node->left !=NULL){
+            q.push({node->left,line-1});
+        }
+        if(node->right!=NULL){
+            q.push({node->right,line+1});
+        }
+    }
+    for(auto it: mp){
+        ans.push_back(it.second);
+    }
+    return ans;
+}
