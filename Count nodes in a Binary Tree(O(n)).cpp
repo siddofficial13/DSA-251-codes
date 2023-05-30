6d3b1adc6af43dd5093ea8c83f45c0f114ba6d7f
Siddharth <128206073+siddofficial13@users.noreply.github.com>
2023-05-30 20:10:58 +0530

    Create Count nodes in a Binary Tree(O(n)).cpp

---
+++ Count nodes in a Binary Tree(O(n)).cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int right(TreeNode *root){
+        int count=0;
+        while(root){
+            count++;
+            root=root->right;
+        }
+        return count;
+    }
+    int left(TreeNode *root){
+        int count=0;
+        while(root){
+            count++;
+            root=root->left;
+        }
+        return count;
+    }
+
+    int countNodes(TreeNode* root) {
+        if(root==NULL)return 0;
+        int lh=left(root);
+        int rh=right(root);
+        if(lh==rh) return (1<<lh)-1;
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
