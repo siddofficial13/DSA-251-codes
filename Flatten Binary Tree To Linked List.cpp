a92c22d9220e098962a23c956b84c31683633cbd
Siddharth <128206073+siddofficial13@users.noreply.github.com>
2023-05-31 11:44:45 +0530

    Create Flatten Binary Tree To Linked List.cpp

---
+++ Flatten Binary Tree To Linked List.cpp
@@ -0,0 +1,70 @@
+class Solution {
+  node * prev = NULL;
+  public:
+    void flatten(node * root) {
+      if (root == NULL) return;
+
+      flatten(root -> right);
+      flatten(root -> left);
+
+      root -> right = prev;
+      root -> left = NULL;
+      prev = root;
+    }
+
+};
+
+class Solution {
+  node * prev = NULL;
+  public:
+   void flatten(node * root) {
+  if (root == NULL) return;
+  stack < node * > st;
+  st.push(root);
+  while (!st.empty()) {
+    node * cur = st.top();
+    st.pop();
+
+    if (cur -> right != NULL) {
+      st.push(cur -> right);
+    }
+    if (cur -> left != NULL) {
+      st.push(cur -> left);
+    }
+    if (!st.empty()) {
+      cur -> right = st.top();
+    }
+    cur -> left = NULL;
+  }
+
+}
+
+
+};
+
+
+class Solution {
+  node * prev = NULL;
+  public:
+   void flatten(node* root) {
+        node* cur = root;
+		while (cur)
+		{
+			if(cur->left)
+			{
+				node* pre = cur->left;
+				while(pre->right)
+				{
+					pre = pre->right;
+				}
+				pre->right = cur->right;
+				cur->right = cur->left;
+				cur->left = NULL;
+			}
+			cur = cur->right;
+		}
+    }
+
+
+
+};
