4269a25fc82c0911ec10b7b85578964efdcdafb3
Siddharth <128206073+siddofficial13@users.noreply.github.com>
2023-06-06 17:16:15 +0530

    Create Bipartite Graph.cpp

---
+++ Bipartite Graph.cpp
@@ -0,0 +1,72 @@
+//using BFS Approach
+class Solution {
+public:
+    bool bfs(int start,vector<int> &color,vector<vector<int>> &graph){
+        queue<int>q;
+        q.push(start);
+        color[start]=0;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(auto it: graph[node]){
+                if(color[it]==-1){
+                    color[it]=!color[node];
+                    q.push(it);
+                }
+                else if(color[it]==color[node]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int>color(n,-1);
+        for(int i=0;i<n;i++){
+            if(color[i]==-1){
+    if(!bfs(i,color,graph)){
+                return false;
+            }
+            }
+        
+        }
+        return true;
+    }
+};
+
+//Using DFS
+class Solution {
+private: 
+    bool dfs(int node, int col, int color[], vector<int> adj[]) {
+        color[node] = col; 
+        
+        // traverse adjacent nodes
+        for(auto it : adj[node]) {
+            // if uncoloured
+            if(color[it] == -1) {
+                if(dfs(it, !col, color, adj) == false) return false; 
+            }
+            // if previously coloured and have the same colour
+            else if(color[it] == col) {
+                return false; 
+            }
+        }
+        
+        return true; 
+    }
+public:
+	bool isBipartite(int V, vector<int>adj[]){
+	    int color[V];
+	    for(int i = 0;i<V;i++) color[i] = -1; 
+	    
+	    // for connected components
+	    for(int i = 0;i<V;i++) {
+	        if(color[i] == -1) {
+	            if(dfs(i, 0, color, adj) == false) 
+	                return false; 
+	        }
+	    }
+	    return true; 
+	}
+};
