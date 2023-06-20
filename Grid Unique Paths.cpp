52925220d156eed2ed7c5d99b22c7108a88f59ac
Siddharth <128206073+siddofficial13@users.noreply.github.com>
2023-06-20 12:52:20 +0530

    Create Grid Unique Paths.cpp

---
+++ Grid Unique Paths.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countWays(int m,int n, vector<vector<int>> & dp){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+                int up=0;
+                int down=0;
+                if(i>0)up=dp[i-1][j];
+                if(j>0)down=dp[i][j-1];
+                dp[i][j]=up + down;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+    int uniquePaths(int m, int n) {
+        
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        return countWays(m,n,dp);
+    }
+};
