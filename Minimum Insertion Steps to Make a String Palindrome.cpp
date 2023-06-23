d452b629305871586c6dcd4f06857d7cec0d1fc6
Siddharth <128206073+siddofficial13@users.noreply.github.com>
2023-06-23 19:47:59 +0530

    Create Minimum Insertion Steps to Make a String Palindrome.cpp

---
+++ Minimum Insertion Steps to Make a String Palindrome.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+       int f(int i,int j,string s, string t,vector<vector<int>> &dp){
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(s[i]==t[j]) return dp[i][j] =1 + f(i-1,j-1,s,t,dp);
+        return dp[i][j] = max(f(i-1,j,s,t,dp),f(i,j-1,s,t,dp));
+    }
+    int longestPalindromeSubseq(string s) {
+        string t=s;
+        reverse(t.begin(),t.end());
+        int n = s.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        for(int i=0;i<=n;i++)dp[i][0]=0;
+        for(int i=0;i<=n;i++)dp[0][i]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][n];
+    }
+    int minInsertions(string s) {
+        return s.size()-longestPalindromeSubseq(s);
+    }
+};
