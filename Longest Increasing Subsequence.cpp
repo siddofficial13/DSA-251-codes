30adef638a78d932fef7329ca0f4b6b2ccf8d1b2
Siddharth <128206073+siddofficial13@users.noreply.github.com>
2023-06-29 15:10:40 +0530

    Update Longest Increasing Subsequence.cpp

--- Longest Increasing Subsequence.cpp
+++ Longest Increasing Subsequence.cpp
@@ -23,16 +23,31 @@ public:
         //     }
         // }
         // return dp[0][0];
-        vector<int>dp(n,1);
-        int maxm = 1;
-        for(int i=0;i<n;i++){
-            for(int prev=0;prev<i;prev++){
-                if(nums[prev]<nums[i]){
-                    dp[i]=max(dp[i],1+dp[prev]);
-                }
+        // vector<int>dp(n,1);
+        // int maxm = 1;
+        // for(int i=0;i<n;i++){
+        //     for(int prev=0;prev<i;prev++){
+        //         if(nums[prev]<nums[i]){
+        //             dp[i]=max(dp[i],1+dp[prev]);
+        //         }
+        //     }
+        //     maxm=max(maxm,dp[i]);
+        // }
+        // return maxm;
+        vector<int>temp;
+        temp.push_back(nums[0]);
+        int len=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]>temp.back()){
+                temp.push_back(nums[i]);
+                len++;
             }
-            maxm=max(maxm,dp[i]);
+            else{
+              int ind = lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+              temp[ind] = nums[i];
+            }
         }
-        return maxm;
+        return len;
     }
+
 };
